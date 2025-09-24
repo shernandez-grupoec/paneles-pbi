d988e46015358a90d02c34451a40f8325f8ed168
hernandezsam <114604996+hernandezsam@users.noreply.github.com>
2025-09-24 17:32:53 -0300

    Se corrigio server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-app.use(express.static("public"));
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const { Pool } = require("pg");
 
 const app = express();
+
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
   secret: "miSecretoPowerBI",
